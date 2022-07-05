b020c692d45ddeb9ab11bd8e46c3789ba0277fc0
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-05 15:32:54 +0100

    Created notesView class and tests, tests passed

---
+++ notesView.test.js
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const NotesModel = require("./notesModel");
+const NotesView = require("./notesView");
+const fs = require("fs");
+
+describe("NotesView", () => {
+  it("displays one note", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote("This is the first Note");
+
+    view.displayNotes();
+    expect(document.body.querySelectorAll("div.note").length).toStrictEqual(1);
+  });
+
+  it("displays multiple notes", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote("This is the first Note");
+    model.addNote("This is the second Note");
+    model.addNote("This is the third Note");
+    view.displayNotes();
+    expect(document.body.querySelectorAll("div.note").length).toStrictEqual(3);
+    expect(document.body.querySelectorAll("#Test").length).toStrictEqual(3);
+  });
+});
